a1140b4c98abdc6cb86454fb719610443a47da18
Vasily93 <markovavasily@gmail.com>
2019-07-17 15:29:47 -0700

    added cellbacks to tests, beforeAll,  baeforeEach, AfterAll

--- test/studios.test.js
+++ test/studios.test.js
@@ -1,8 +1,21 @@
 const app = require('../lib/app');
 const request = require('supertest');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
 
 
 describe('testig sudio routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });   
   it('creates studio with PUT', () => {
     return request(app)
       .post('/api/v1/studios')
