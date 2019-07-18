792655f4ffeeabd2705bc4466d8eb613cf4164f6
Vasily93 <markovavasily@gmail.com>
2019-07-18 10:54:15 -0700

    test to get an actor by id

--- test/actor.test.js
+++ test/actor.test.js
@@ -38,17 +38,37 @@ describe('tesind actor routes', () => {
   });
 
   it('getting all actor with GET', async() => {
-    const actors = await Actor.create({ name: 'Brad Pit' });
+    const actor = await Actor.create({ name: 'Brad Pit' });
 
     return request(app)
       .get('/api.v1/actors')
       .then(res => {
-        const actorsJSON = JSON.parse(JSON.stringify(actors));
+        const actorsJSON = JSON.parse(JSON.stringify(actor));
         actorsJSON.forEach(actor => {
           expect(res.body).toContainEqual({
             _id: actor._id,
             name: actor.name
           });
+        });
+      });
+  });
+
+  it('get actor by id', async() => {
+    const actor = await Actor.create({ 
+      name: 'Brad Pit',
+      DOB: '1993-09-08',
+      POB: 'USA'
+    });
+
+    return request(app)
+      .get(`/api/v1/actors/${actor._id}`)
+      .then(res => {
+        expect.any(res.body).toEqual({
+          _id: expect.any(String),
+          name:  'Brad Pit',
+          DOB: '1993-09-08',
+          POB: 'USA',
+          __v: 0
         });
       });
   });
